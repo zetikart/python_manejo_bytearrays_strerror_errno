0fab62865c230e2aad35f1fc8fceb05f62e9ec7e
zetikarthack <114775443+zetikart@users.noreply.github.com>
2024-12-10 07:21:08 -0300

    Create manejo_archivos_binarios.py
    📂 Manejo de Archivos Binarios en Python 🐍
    ¡Bienvenido a mi repositorio! 🚀 Aquí encontrarás un sencillo pero poderoso script en Python que demuestra cómo manejar archivos binarios. Este script lee los primeros 5 bytes de un archivo binario y los imprime en formato hexadecimal. Ideal para quienes quieren aprender a trabajar con datos binarios en Python de manera eficiente y segura. 😊🔧

---
+++ manejo_archivos_binarios.py
@@ -0,0 +1,13 @@
+from os import strerror
+
+try:
+    binary_file = open('file.bin', 'rb')
+    data = bytearray(binary_file.read(5))
+    binary_file.close()
+
+    for b in data:
+        print(hex(b), end=' ')
+
+except IOError as e:
+    print("Se produjo un error de E/S:", strerror(e.errno))
+    
