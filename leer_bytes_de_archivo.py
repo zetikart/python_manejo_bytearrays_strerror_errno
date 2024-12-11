23b8f3bf756b00351d4b9e8e9c0e6f03fb1c9a4b
zetikarthack <114775443+zetikart@users.noreply.github.com>
2024-12-10 21:35:23 -0300

    Create leer_bytes_de_archivo.py
     puedes leer los bytes del archivo file.bin que acabas de escribir y luego imprimir esos bytes en formato hexadecimal
    
    🐍 Escritura y Lectura de bytearray en Python 🖥️
    ¡Bienvenido a mi repositorio! 🚀 Este proyecto contiene un script en Python que demuestra cómo escribir y leer datos binarios utilizando bytearray. Primero, escribimos un bytearray en un archivo binario y luego leemos ese archivo para imprimir los bytes en formato hexadecimal. 😊🔧
    
    
    📋 ¿Qué Hace Este Código?
    Creación de un bytearray:
    
    data = bytearray(10): Crea un bytearray de tamaño 10, inicializado con ceros.
    
    Inicialización de Valores:
    
    for i in range(len(data)): data[i] = 10 + i: Llena el bytearray con valores incrementales comenzando desde 10.
    
    Escritura en Archivo Binario:
    
    bf = open('file.bin', 'wb'): Abre (o crea) el archivo file.bin en modo de escritura binaria.
    
    bf.write(data): Escribe el bytearray en el archivo.
    
    bf.close(): Cierra el archivo.
    
    Lectura desde Archivo Binario:
    
    bf = open('file.bin', 'rb'): Abre el archivo file.bin en modo de lectura binaria.
    
    data_read = bytearray(bf.read()): Lee el contenido del archivo y lo almacena en un nuevo bytearray.
    
    bf.close(): Cierra el archivo.
    
    Impresión del bytearray en Hexadecimal:
    
    for b in data_read: print(hex(b), end=' '): Itera sobre cada byte en el bytearray leído y lo imprime en formato hexadecimal.
    
    🛠️ Funcionalidades
    Creación y Manipulación de bytearray: Aprende a crear y manipular bytearray, una estructura de datos esencial para manejar datos binarios en Python.
    
    Escritura de Datos Binarios: Escribe bytearray en un archivo binario.
    
    Lectura de Datos Binarios: Lee datos binarios desde un archivo y visualiza su contenido en formato hexadecimal.
    
    Manejo de Errores: Implementa una gestión robusta de errores para manejar problemas de entrada/salida.

---
+++ leer_bytes_de_archivo.py
@@ -0,0 +1,16 @@
+from os import strerror
+
+data = bytearray(10)
+
+for i in range(len(data)):
+    data[i] = 10 + i
+
+try:
+    bf = open('file.bin', 'wb')
+    bf.write(data)
+    bf.close()
+except IOError as e:
+    print("Se produjo un error de E/S:", strerror(e.errno))
+
+
+# Ingresa aquí el código que lee los bytes del stream.
