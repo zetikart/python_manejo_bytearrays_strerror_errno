edc6df7cc7bf799b41d30ee2faf03763412ae5d3
zetikarthack <114775443+zetikart@users.noreply.github.com>
2024-12-12 19:50:20 -0300

    Create escritura_de_archivo_texto.py
    🐍 Escritura de Archivos de Texto en Python 🖥️
    ¡Bienvenido a mi repositorio! 🚀 Este proyecto contiene un script en Python que demuestra cómo escribir líneas en un archivo de texto y manejar posibles errores de entrada/salida. Este script es ideal para quienes desean aprender a manipular archivos de texto en Python de manera eficiente. 😊🔧
    📋 ¿Qué Hace Este Código?
    Importar strerror:
    
    from os import strerror: Importa la función strerror del módulo os, que se utiliza para proporcionar mensajes de error descriptivos.
    
    Abrir el Archivo en Modo de Escritura:
    
    file = open('newtext.txt', 'wt'): Abre (o crea) el archivo newtext.txt en modo de escritura de texto (wt).
    
    Escribir Líneas en el Archivo:
    
    for i in range(10): Itera del 0 al 9.
    
    file.write("línea #" + str(i+1) + "\n"): Escribe cada línea numerada en el archivo. La función str(i+1) convierte el número en una cadena para concatenarlo con el texto.
    
    Cerrar el Archivo:
    
    file.close(): Cierra el archivo después de escribir las líneas.
    
    Manejo de Errores:
    
    except IOError as e: Captura cualquier excepción de entrada/salida (IOError) que pueda ocurrir durante la apertura o escritura del archivo.
    
    print("Se produjo un error de E/S: ", strerror(e.errno)): Imprime un mensaje de error en la consola con la descripción del error.
    
    Ejemplo de Salida en el Archivo
    El contenido del archivo newtext.txt será algo como:
    
    línea #1
    línea #2
    línea #3
    línea #4
    línea #5
    línea #6
    línea #7
    línea #8
    línea #9
    línea #10
    🛠️ Funcionalidades
    Escritura en Archivos de Texto: Aprende a escribir líneas en un archivo de texto en Python.
    
    Manejo de Errores de Entrada/Salida: Implementa una gestión robusta de errores para manejar problemas de entrada/salida y proporcionar mensajes de error descriptivos.

---
+++ escritura_de_archivo_texto.py
@@ -0,0 +1,9 @@
+from os import strerror
+
+try:
+    file = open('newtext.txt', 'wt')
+    for i in range(10):
+        file.write("línea #" + str(i+1) + "\n")
+    file.close()
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerror(e.errno))
