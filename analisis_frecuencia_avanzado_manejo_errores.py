fbabaa7c1dde8ff4d6fca2394ef763257a71994f
zetikarthack <114775443+zetikart@users.noreply.github.com>
2024-12-12 10:21:05 -0300

    Create analisis_frecuencia_avanzado_manejo_errores.py
    📊 Análisis de Frecuencia de Letras en Archivos de Texto 🐍
    ¡Bienvenido a mi repositorio! 🚀 Este proyecto contiene un script en Python que analiza la frecuencia de letras en un archivo de texto. El script cuenta las ocurrencias de cada letra latina, las convierte a minúsculas y muestra los resultados en la consola. 😊🔧
    
    
    📋 ¿Qué Hace Este Código?
    Inicialización de Contadores:
    
    counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}: Crea un diccionario con todas las letras del alfabeto como claves y las inicializa en 0.
    
    Entrada del Usuario:
    
    file_name = input("Ingresa el nombre del archivo a analizar: "): Solicita al usuario el nombre del archivo a analizar.
    
    Intento de Leer el Archivo:
    
    try ... except: Maneja excepciones de entrada/salida (IOError).
    
    file = open(file_name, "rt"): Abre el archivo en modo de lectura de texto.
    
    Contar Caracteres:
    
    Itera a través de cada línea y cada carácter del archivo.
    
    if char.isalpha(): counters[char.lower()] += 1: Incrementa el contador para cada letra (ignora los caracteres no alfabéticos).
    
    Imprimir Resultados:
    
    Imprimir Contadores:
    
    Itera sobre las claves del diccionario counters.
    
    print(char, '->', c): Imprime cada letra y su frecuencia si la frecuencia es mayor que 0.
    
    Ejemplo de Salida
    La salida del script será algo como:
    
    Ingresa el nombre del archivo a analizar: ejemplo.txt
    a -> 10
    b -> 5
    c -> 7
    ...
    🛠️ Funcionalidades
    Análisis de Frecuencia de Letras: Analiza y cuenta las ocurrencias de cada letra en un archivo de texto.
    
    Visualización de Resultados: Imprime un informe simple en la consola mostrando la frecuencia de cada letra.
    
    Manejo de Errores: Implementa una gestión robusta de errores para manejar problemas de entrada/salida.

---
+++ analisis_frecuencia_avanzado_manejo_errores.py
@@ -0,0 +1,23 @@
+from os import strerror
+
+# Inicializa 26 contadores para cada letra latina.
+# Nota: hemos usado una comprensión para esto.
+counters = {chr(ch): 0 for ch in range(ord('a'), ord('z') + 1)}
+file_name = input("Ingresa el nombre del archivo a analizar: ")
+try:
+    file = open(file_name, "rt")
+    for line in file:
+        for char in line:
+            # Si es una letra...
+            if char.isalpha():
+                # ... lo trataremos en minúsculas y actualizaremos el contador apropiado.
+                counters[char.lower()] += 1
+    file.close()
+    # Imprimamos los contadores.
+    for char in counters.keys():
+        c = counters[char]
+        if c > 0:
+            print(char, '->', c)
+except IOError as e:
+    print("Se produjo un error de E/S: ", strerror(e.errno))
+    
